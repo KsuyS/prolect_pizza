0857638661ec609270f1f790b2d33a0a561264ab
[KsuyS] <[solovevak105@gmail.com]>
2024-06-29 02:11:21 +0300

    Подправила вёрстку

---
+++ public/scrypt/register-user.js
@@ -0,0 +1,48 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const data =
+    {
+        image: '',
+    }
+    const imageInput = document.getElementById('avatar_path');
+
+    function onAvatarPathInputChange(event) {
+        const file = event.target.files[0];
+        readFileAsBase64(file, (result) => {
+            data.image = result;
+            invalidateAvatarPathPreview()
+        });
+        document.querySelectorAll('.block__upload-new-button')[0].classList.add('block__upload-new-button_download')
+        document.querySelectorAll('.block__remove-button')[0].classList.add('block__remove-button_download')
+    }
+
+   
+
+    function readFileAsBase64(file, onload) {
+        const reader = new FileReader();
+        reader.addEventListener("load", () => {
+            onload(reader.result);
+        }, false,);
+        reader.readAsDataURL(file);
+    }
+
+    function initEventListener() {
+        imageInput.addEventListener('change', onAvatarPathInputChange)
+    }
+
+    function invalidateAvatarPathPreview() {
+        const PreviewAvatarPath = document.querySelector('.block__avatar-preview')
+        PreviewAvatarPath.src = data.image
+    }
+
+    initEventListener();
+
+
+    document.querySelectorAll('.block__remove-button')[0].onclick = deleteImage;
+
+    function deleteImage() {
+        const PreviewAvatarPath = document.querySelector('.block__avatar-preview')
+        PreviewAvatarPath.src = "/image/avatar.png";
+        document.querySelectorAll('.block__upload-new-button')[0].classList.remove('block__upload-new-button_download')
+        document.querySelectorAll('.block__remove-button')[0].classList.remove('block__remove-button_download')
+    }
+});
